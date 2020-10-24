55f52f7006f20f3d552199a03d6b541b19b7df9f
Technixo LTD <73162417+technixo@users.noreply.github.com>
2020-10-24 17:15:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
-const port = 3003
+const port = 3000
 const parseFunction = require('./parse-firestore-to-dart')
 
 
